cbf74a9522f328f843dc5c0a8dc7b8d35f8e67a8
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2022-07-13 12:30:15 +0500

    task 22

---
+++ task_22.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	a := big.NewInt(2 << 25)
+	b := big.NewInt(2<<25 + 10)
+	fmt.Printf("Число a: %v\t b: %v\n", a, b)
+
+	res := big.NewInt(0)
+
+	res.Add(a, b)
+	fmt.Printf("%v + %v = %v\n", a, b, res)
+
+	res.Sub(a, b)
+	fmt.Printf("%v - %v = %v\n", a, b, res)
+
+	res.Mul(a, b)
+	fmt.Printf("%v * %v = %v\n", a, b, res)
+
+	res.Quo(a, b)
+	fmt.Printf("%v / %v = %v\n", a, b, res)
+}
